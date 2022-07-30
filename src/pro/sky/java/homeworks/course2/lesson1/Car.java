11cf7ff55f145a674e95cb93e8a0451b821feab6
Kirill <K.katyshev1503@gmail.com>
2022-07-30 16:30:23 +0300

    Выполнил ДЗ

--- src/pro/sky/java/homeworks/course2/lesson1/Car.java
+++ src/pro/sky/java/homeworks/course2/lesson1/Car.java
@@ -5,15 +5,12 @@ public class Car extends WeelanleTransport implements Service {
     public Car(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
-
     private void updateTyre() {
         System.out.println("Меняем покрышку у машины");
     }
-
     private void checkEngine() {
         System.out.println("Проверяем двигатель у машины");
     }
-
     @Override
     public void check() {
         System.out.println("Обслуживаем машину " + getModelName());
--- src/pro/sky/java/homeworks/course2/lesson1/Service.java
+++ src/pro/sky/java/homeworks/course2/lesson1/Service.java
@@ -1,5 +1,6 @@
 package pro.sky.java.homeworks.course2.lesson1;
 
 public interface Service {
+
     void check();
 }
